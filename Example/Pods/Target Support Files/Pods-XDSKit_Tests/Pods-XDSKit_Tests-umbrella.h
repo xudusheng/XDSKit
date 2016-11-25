f6ed97de1176ff77c0a95f20f808d7019770473e
xudusheng <597132100@qq.com>
2016-11-25 15:19:49 +0800

    Initial Commit of XDSKit
    Signed-off-by: xudusheng <597132100@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-XDSKit_Tests/Pods-XDSKit_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_XDSKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XDSKit_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/XDSKit/XDSKit-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "UIViewController+HideNavigationBar.h"
+
+FOUNDATION_EXPORT double XDSKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char XDSKitVersionString[];
+
